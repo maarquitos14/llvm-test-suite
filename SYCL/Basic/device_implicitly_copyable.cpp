6fc1e34fb801881d16bbdc131d86b969b73ff1aa
Maronas, Marcos <marcos.maronas@intel.com>
2023-02-09 05:54:49 -0800

    Moving common code to a function.
    Signed-off-by: Maronas, Marcos <marcos.maronas@intel.com>

--- SYCL/Basic/device_implicitly_copyable.cpp
+++ SYCL/Basic/device_implicitly_copyable.cpp
@@ -24,39 +24,42 @@ struct ACopyable {
 
 template <> struct sycl::is_device_copyable<ACopyable> : std::true_type {};
 
+template <typename DataT, size_t ArrSize>
+void CaptureAndCopy(const DataT *data_arr, const DataT &data_scalar,
+                    DataT *result_arr, DataT *result_scalar, sycl::queue &q) {
+  sycl::buffer<DataT, 1> buf_arr{result_arr, sycl::range<1>(ArrSize)};
+  sycl::buffer<DataT, 1> buf_scalar{result_scalar, sycl::range<1>(1)};
+
+  q.submit([&](sycl::handler &cgh) {
+    auto acc_arr = sycl::accessor{buf_arr, cgh, sycl::read_write};
+    auto acc_scalar = sycl::accessor{buf_scalar, cgh, sycl::read_write};
+    cgh.single_task([=]() {
+      for (auto i = 0; i < ArrSize; i++) {
+        acc_arr[i] = data_arr[i];
+      }
+      acc_scalar[0] = data_scalar;
+    });
+  });
+}
+
 int main() {
   constexpr size_t arr_size = 5;
   constexpr int ref_val = 14;
   sycl::queue q;
   {
-    std::pair<int, float> pair_arr[arr_size];
-    std::pair<int, float> pair{ref_val, ref_val};
-    std::pair<int, float> result_pair_arr[arr_size];
-    std::pair<int, float> result_pair;
+    using DataT = std::pair<int, float>;
+    DataT pair_arr[arr_size];
+    DataT pair{ref_val, ref_val};
+    DataT result_pair_arr[arr_size];
+    DataT result_pair;
 
     for (auto i = 0; i < arr_size; i++) {
       pair_arr[i].first = i;
       pair_arr[i].second = i;
     }
 
-    {
-      sycl::buffer<std::pair<int, float>, 1> buf_pair_arr{
-          result_pair_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::pair<int, float>, 1> buf_pair{&result_pair,
-                                                      sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_pair_arr =
-             sycl::accessor{buf_pair_arr, cgh, sycl::read_write};
-         auto acc_pair = sycl::accessor{buf_pair, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_pair_arr[i] = pair_arr[i];
-           }
-           acc_pair[0] = pair;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(pair_arr, pair, result_pair_arr,
+                                    &result_pair, q);
 
     for (auto i = 0; i < arr_size; i++) {
       assert(result_pair_arr[i].first == i);
@@ -66,34 +69,19 @@ int main() {
   }
 
   {
-    std::pair<ACopyable, float> pair_arr[arr_size];
-    std::pair<ACopyable, float> pair{ACopyable(ref_val), ref_val};
-    std::pair<ACopyable, float> result_pair_arr[arr_size];
-    std::pair<ACopyable, float> result_pair;
+    using DataT = std::pair<ACopyable, float>;
+    DataT pair_arr[arr_size];
+    DataT pair{ACopyable(ref_val), ref_val};
+    DataT result_pair_arr[arr_size];
+    DataT result_pair;
 
     for (auto i = 0; i < arr_size; i++) {
       pair_arr[i].first = ACopyable(i);
       pair_arr[i].second = i;
     }
 
-    {
-      sycl::buffer<std::pair<ACopyable, float>, 1> buf_pair_arr{
-          result_pair_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::pair<ACopyable, float>, 1> buf_pair{&result_pair,
-                                                            sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_pair_arr =
-             sycl::accessor{buf_pair_arr, cgh, sycl::read_write};
-         auto acc_pair = sycl::accessor{buf_pair, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_pair_arr[i] = pair_arr[i];
-           }
-           acc_pair[0] = pair;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(pair_arr, pair, result_pair_arr,
+                                    &result_pair, q);
 
     for (auto i = 0; i < arr_size; i++) {
       assert(result_pair_arr[i].first.i == i);
@@ -103,10 +91,11 @@ int main() {
   }
 
   {
-    std::tuple<int, float, bool> tuple_arr[arr_size];
-    std::tuple<int, float, bool> tuple{ref_val, ref_val, true};
-    std::tuple<int, float, bool> result_tuple_arr[arr_size];
-    std::tuple<int, float, bool> result_tuple;
+    using DataT = std::tuple<int, float, bool>;
+    DataT tuple_arr[arr_size];
+    DataT tuple{ref_val, ref_val, true};
+    DataT result_tuple_arr[arr_size];
+    DataT result_tuple;
 
     for (auto i = 0; i < arr_size; i++) {
       auto &t = tuple_arr[i];
@@ -115,24 +104,8 @@ int main() {
       std::get<2>(t) = true;
     }
 
-    {
-      sycl::buffer<std::tuple<int, float, bool>, 1> buf_tuple_arr{
-          result_tuple_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::tuple<int, float, bool>, 1> buf_tuple{
-          &result_tuple, sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_tuple_arr =
-             sycl::accessor{buf_tuple_arr, cgh, sycl::read_write};
-         auto acc_tuple = sycl::accessor{buf_tuple, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_tuple_arr[i] = tuple_arr[i];
-           }
-           acc_tuple[0] = tuple;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(tuple_arr, tuple, result_tuple_arr,
+                                    &result_tuple, q);
 
     for (auto i = 0; i < arr_size; i++) {
       auto t = result_tuple_arr[i];
@@ -146,10 +119,11 @@ int main() {
   }
 
   {
-    std::tuple<ACopyable, float, bool> tuple_arr[arr_size];
-    std::tuple<ACopyable, float, bool> tuple{ACopyable(ref_val), ref_val, true};
-    std::tuple<ACopyable, float, bool> result_tuple_arr[arr_size];
-    std::tuple<ACopyable, float, bool> result_tuple;
+    using DataT = std::tuple<ACopyable, float, bool>;
+    DataT tuple_arr[arr_size];
+    DataT tuple{ACopyable(ref_val), ref_val, true};
+    DataT result_tuple_arr[arr_size];
+    DataT result_tuple;
 
     for (auto i = 0; i < arr_size; i++) {
       auto &t = tuple_arr[i];
@@ -158,24 +132,8 @@ int main() {
       std::get<2>(t) = true;
     }
 
-    {
-      sycl::buffer<std::tuple<ACopyable, float, bool>, 1> buf_tuple_arr{
-          result_tuple_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::tuple<ACopyable, float, bool>, 1> buf_tuple{
-          &result_tuple, sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_tuple_arr =
-             sycl::accessor{buf_tuple_arr, cgh, sycl::read_write};
-         auto acc_tuple = sycl::accessor{buf_tuple, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_tuple_arr[i] = tuple_arr[i];
-           }
-           acc_tuple[0] = tuple;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(tuple_arr, tuple, result_tuple_arr,
+                                    &result_tuple, q);
 
     for (auto i = 0; i < arr_size; i++) {
       auto t = result_tuple_arr[i];
@@ -189,10 +147,11 @@ int main() {
   }
 
   {
-    std::variant<int, float, bool> variant_arr[arr_size];
-    std::variant<int, float, bool> variant{14};
-    std::variant<int, float, bool> result_variant_arr[arr_size];
-    std::variant<int, float, bool> result_variant;
+    using DataT = std::variant<int, float, bool>;
+    DataT variant_arr[arr_size];
+    DataT variant{14};
+    DataT result_variant_arr[arr_size];
+    DataT result_variant;
 
     constexpr int variant_size = 3;
     for (auto i = 0; i < arr_size; i++) {
@@ -207,24 +166,8 @@ int main() {
       }
     }
 
-    {
-      sycl::buffer<std::variant<int, float, bool>, 1> buf_variant_arr{
-          result_variant_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::variant<int, float, bool>, 1> buf_variant{
-          &result_variant, sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_variant_arr =
-             sycl::accessor{buf_variant_arr, cgh, sycl::read_write};
-         auto acc_variant = sycl::accessor{buf_variant, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_variant_arr[i] = variant_arr[i];
-           }
-           acc_variant[0] = variant;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(variant_arr, variant, result_variant_arr,
+                                    &result_variant, q);
 
     for (auto i = 0; i < arr_size; i++) {
       auto v = result_variant_arr[i];
@@ -241,10 +184,11 @@ int main() {
   }
 
   {
-    std::variant<ACopyable> variant_arr[arr_size];
-    std::variant<ACopyable> variant;
-    std::variant<ACopyable> result_variant_arr[arr_size];
-    std::variant<ACopyable> result_variant;
+    using DataT = std::variant<ACopyable>;
+    DataT variant_arr[arr_size];
+    DataT variant;
+    DataT result_variant_arr[arr_size];
+    DataT result_variant;
     q.submit([&](sycl::handler &cgh) {
        cgh.single_task([=]() {
          // std::variant with complex types relies on virtual functions, so
@@ -256,10 +200,11 @@ int main() {
   }
 
   {
-    std::array<int, arr_size> arr_arr[arr_size];
-    std::array<int, arr_size> arr;
-    std::array<int, arr_size> result_arr_arr[arr_size];
-    std::array<int, arr_size> result_arr;
+    using DataT = std::array<int, arr_size>;
+    DataT arr_arr[arr_size];
+    DataT arr;
+    DataT result_arr_arr[arr_size];
+    DataT result_arr;
 
     for (auto i = 0; i < arr_size; i++) {
       auto &a = arr_arr[i];
@@ -269,23 +214,8 @@ int main() {
       arr[i] = i;
     }
 
-    {
-      sycl::buffer<std::array<int, arr_size>, 1> buf_arr_arr{
-          result_arr_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::array<int, arr_size>, 1> buf_arr{&result_arr,
-                                                         sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_arr_arr = sycl::accessor{buf_arr_arr, cgh, sycl::read_write};
-         auto acc_arr = sycl::accessor{buf_arr, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_arr_arr[i] = arr_arr[i];
-           }
-           acc_arr[0] = arr;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(arr_arr, arr, result_arr_arr, &result_arr,
+                                    q);
 
     for (auto i = 0; i < arr_size; i++) {
       auto a = result_arr_arr[i];
@@ -297,10 +227,11 @@ int main() {
   }
 
   {
-    std::array<ACopyable, arr_size> arr_arr[arr_size];
-    std::array<ACopyable, arr_size> arr;
-    std::array<ACopyable, arr_size> result_arr_arr[arr_size];
-    std::array<ACopyable, arr_size> result_arr;
+    using DataT = std::array<ACopyable, arr_size>;
+    DataT arr_arr[arr_size];
+    DataT arr;
+    DataT result_arr_arr[arr_size];
+    DataT result_arr;
 
     for (auto i = 0; i < arr_size; i++) {
       auto &a = arr_arr[i];
@@ -310,23 +241,8 @@ int main() {
       arr[i] = ACopyable(i);
     }
 
-    {
-      sycl::buffer<std::array<ACopyable, arr_size>, 1> buf_arr_arr{
-          result_arr_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::array<ACopyable, arr_size>, 1> buf_arr{
-          &result_arr, sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_arr_arr = sycl::accessor{buf_arr_arr, cgh, sycl::read_write};
-         auto acc_arr = sycl::accessor{buf_arr, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_arr_arr[i] = arr_arr[i];
-           }
-           acc_arr[0] = arr;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(arr_arr, arr, result_arr_arr, &result_arr,
+                                    q);
 
     for (auto i = 0; i < arr_size; i++) {
       auto a = result_arr_arr[i];
@@ -338,33 +254,19 @@ int main() {
   }
 
   {
-    std::optional<int> opt_arr[arr_size];
-    std::optional<int> opt;
-    std::optional<int> result_opt_arr[arr_size];
-    std::optional<int> result_opt;
+    using DataT = std::optional<int>;
+    DataT opt_arr[arr_size];
+    DataT opt;
+    DataT result_opt_arr[arr_size];
+    DataT result_opt;
 
     for (auto i = 0; i < arr_size; i++) {
       opt_arr[i] = i;
     }
     opt = ref_val;
 
-    {
-      sycl::buffer<std::optional<int>, 1> buf_opt_arr{result_opt_arr,
-                                                      sycl::range<1>(arr_size)};
-      sycl::buffer<std::optional<int>, 1> buf_opt{&result_opt,
-                                                  sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_opt_arr = sycl::accessor{buf_opt_arr, cgh, sycl::read_write};
-         auto acc_opt = sycl::accessor{buf_opt, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_opt_arr[i] = opt_arr[i];
-           }
-           acc_opt[0] = opt;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(opt_arr, opt, result_opt_arr, &result_opt,
+                                    q);
 
     for (auto i = 0; i < arr_size; i++) {
       assert(result_opt_arr[i] == i);
@@ -374,33 +276,19 @@ int main() {
   }
 
   {
-    std::optional<ACopyable> opt_arr[arr_size];
-    std::optional<ACopyable> opt;
-    std::optional<ACopyable> result_opt_arr[arr_size];
-    std::optional<ACopyable> result_opt;
+    using DataT = std::optional<ACopyable>;
+    DataT opt_arr[arr_size];
+    DataT opt;
+    DataT result_opt_arr[arr_size];
+    DataT result_opt;
 
     for (auto i = 0; i < arr_size; i++) {
       opt_arr[i] = ACopyable(i);
     }
     opt = ACopyable(ref_val);
 
-    {
-      sycl::buffer<std::optional<ACopyable>, 1> buf_opt_arr{
-          result_opt_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::optional<ACopyable>, 1> buf_opt{&result_opt,
-                                                        sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_opt_arr = sycl::accessor{buf_opt_arr, cgh, sycl::read_write};
-         auto acc_opt = sycl::accessor{buf_opt, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_opt_arr[i] = opt_arr[i];
-           }
-           acc_opt[0] = opt;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(opt_arr, opt, result_opt_arr, &result_opt,
+                                    q);
 
     for (auto i = 0; i < arr_size; i++) {
       assert(result_opt_arr[i]->i == i);
@@ -410,37 +298,21 @@ int main() {
   }
 
   {
+    using DataT = std ::string_view;
     std::string strv_arr_val[arr_size];
     std::string strv_val{std::to_string(ref_val)};
-    std::string_view strv_arr[arr_size];
-    std::string_view strv{strv_val};
-    std::string_view result_strv_arr[arr_size];
-    std::string_view result_strv;
+    DataT strv_arr[arr_size];
+    DataT strv{strv_val};
+    DataT result_strv_arr[arr_size];
+    DataT result_strv;
 
     for (auto i = 0; i < arr_size; i++) {
       strv_arr_val[i] = std::to_string(i);
       strv_arr[i] = std::string_view{strv_arr_val[i]};
     }
 
-    {
-      sycl::buffer<std::string_view, 1> buf_string_view_arr{
-          result_strv_arr, sycl::range<1>(arr_size)};
-      sycl::buffer<std::string_view, 1> buf_string_view{&result_strv,
-                                                        sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_string_view_arr =
-             sycl::accessor{buf_string_view_arr, cgh, sycl::read_write};
-         auto acc_string_view =
-             sycl::accessor{buf_string_view, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_string_view_arr[i] = strv_arr[i];
-           }
-           acc_string_view[0] = strv;
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(strv_arr, strv, result_strv_arr,
+                                    &result_strv, q);
 
     for (auto i = 0; i < arr_size; i++) {
       assert(result_strv_arr[i] == std::to_string(i));
@@ -451,57 +323,49 @@ int main() {
 
 #if __cpp_lib_span >= 202002
   {
+    using DataT = std::span<int>;
     std::vector<int> v(arr_size);
-    std::span<int> s{v.data(), arr_size};
-    std::span<int> result_s{v.data(), arr_size};
+    DataT s_arr[arr_size];
+    DataT s{v.data(), arr_size};
+    DataT result_s_arr[arr_size];
+    DataT result_s{v.data(), arr_size};
 
     for (auto i = 0; i < arr_size; i++) {
       s[i] = i;
+      s_arr[i] = s;
     }
 
-    {
-      std::buffer<std::span<int>, 1> buf_span{&result_s, std::range<1>(1)};
-
-      q.submit([&](std::handler &cgh) {
-         auto acc_span_arr = std::accessor{buf_span, cgh, std::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_span_arr[0][i] = s[i];
-           }
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(s_arr, s, result_s_arr, &result_s, q);
 
     for (auto i = 0; i < arr_size; i++) {
       assert(result_s[i] == i);
+      for (auto j = 0; j < arr_size; j++) {
+        assert(result_s_arr[i][j] == j);
+      }
     }
   }
 #endif
 
   {
+    using DataT = sycl::span<int>;
     std::vector<int> v(arr_size);
-    sycl::span<int> s{v.data(), arr_size};
-    sycl::span<int> result_s{v.data(), arr_size};
+    DataT s_arr[arr_size];
+    DataT s{v.data(), arr_size};
+    DataT result_s_arr[arr_size];
+    DataT result_s{v.data(), arr_size};
 
     for (auto i = 0; i < arr_size; i++) {
       s[i] = i;
+      s_arr[i] = s;
     }
 
-    {
-      sycl::buffer<sycl::span<int>, 1> buf_span{&result_s, sycl::range<1>(1)};
-
-      q.submit([&](sycl::handler &cgh) {
-         auto acc_span_arr = sycl::accessor{buf_span, cgh, sycl::read_write};
-         cgh.single_task([=]() {
-           for (auto i = 0; i < arr_size; i++) {
-             acc_span_arr[0][i] = s[i];
-           }
-         });
-       }).wait_and_throw();
-    }
+    CaptureAndCopy<DataT, arr_size>(s_arr, s, result_s_arr, &result_s, q);
 
     for (auto i = 0; i < arr_size; i++) {
       assert(result_s[i] == i);
+      for (auto j = 0; j < arr_size; j++) {
+        assert(result_s_arr[i][j] == j);
+      }
     }
   }
 
