9196a965794d714e022b73ef7337565ba6300070
Maronas, Marcos <marcos.maronas@intel.com>
2023-03-14 08:06:37 -0700

    Fixes clang-format issue.
    Signed-off-by: Maronas, Marcos <marcos.maronas@intel.com>

--- SYCL/AtomicRef/atomic_fence_capabilities.h
+++ SYCL/AtomicRef/atomic_fence_capabilities.h
@@ -6,13 +6,13 @@
 using namespace sycl;
 
 bool is_supported_order(const std::vector<memory_order> &capabilities,
-                  memory_order mem_order) {
+                        memory_order mem_order) {
   return std::find(capabilities.begin(), capabilities.end(), mem_order) !=
          capabilities.end();
 }
 
 bool is_supported_scope(const std::vector<memory_scope> &capabilities,
-                  memory_scope mem_scope) {
+                        memory_scope mem_scope) {
   return std::find(capabilities.begin(), capabilities.end(), mem_scope) !=
          capabilities.end();
 }
